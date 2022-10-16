8da063f5847527534dae123d19b0e438de0019c4
PranavJv3 <115917259+PranavJv3@users.noreply.github.com>
2022-10-16 12:45:31 +0530

    Create code_4.cpp

---
+++ code_4.cpp
@@ -0,0 +1,14 @@
+// battery low problem solution
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,t=0;
+    cin>>t;
+	while(t--){
+	    cin>>n;
+	    cout<<((n<=15)?"Yes\n":"No\n") ;
+	}
+	return 0;
+}
