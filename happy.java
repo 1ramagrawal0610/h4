73a413b81a10a31b404fc04eb6e613bf53732691
ram276 <73056932+ram276@users.noreply.github.com>
2022-10-16 12:37:56 +0530

    Create happy.java

---
+++ happy.java
@@ -0,0 +1,14 @@
+public class SumNatural {
+
+    public static void main(String[] args) {
+
+        int num = 100, sum = 0;
+
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
